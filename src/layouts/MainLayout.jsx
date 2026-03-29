65721d332382709cdafac325f4f94cc620361dcb
Andrews <andrewsdevaraj53561@gmail.com>
2026-03-29 17:15:30 +0530

    Invoice Generation with search Values

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
-import Sidebar from "../components/SideBar";
+import Sidebar from "../components/Sidebar";
 import "../styles/layout.css";
 
 export default function MainLayout() {
